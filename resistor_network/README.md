8f049495814b7395aafc111d87ed04544133880f
reps <marco@reps.cc>
2023-01-01 11:50:00 +0100

    asdf

---
+++ resistor_network/README.md
@@ -0,0 +1,9 @@
+## Scope
+
+This is an adapter for up to 8 SMD resistors to fit into a DIP16 footprint. Using this board one could assemble an all-in-one LTZ/ADR configuration and buffering resistor network out of good quality SMD thin film resistors such as RN73. When using a thermally conductive PCB substrate such as Al, Cu, AlN or Al2O3 the resistors are thermally coupled which can be an advantage. When populating the crystal heater circuit and providing it with power, Vref and GND, it will thermally stabilize the resistor networks temperature at around 40°C.
+
+## Result
+
+I have performed only one superficial test with such a network so far and it didn't look as good as the monolithic 8xthin film array method.
+
+![serviervorschlag](https://github.com/marcoreps/ADRmu/raw/main/images/cu_pcb_rn.jpg)
--- software/adrmu_read_temp.py
+++ software/adrmu_read_temp.py
@@ -1,45 +1,25 @@
-class TMP117:
-    
-    i2c_ch = 1
-    reg_temp = 0x00
-    reg_config = 0x01
-    bus = smbus.SMBus(i2c_ch)
-    readable = True
-    read_val = 0.0
-    
-    def is_readable(self):
-        return self.readable
-    
-    def __init__(self, address, title):
-        self.title = title
-        logging.debug(self.title+' init started')
-        self.i2c_address = address
-        val = self.bus.read_i2c_block_data(self.i2c_address, self.reg_config, 2)
-        val[1] = val[1] & 0b00111111
-        val[1] = val[1] | (0b10 << 6)
-        self.bus.write_i2c_block_data(self.i2c_address, self.reg_config, val)
-        val = self.bus.read_i2c_block_data(self.i2c_address, self.reg_config, 2)
-        
-        
-    def measure(self):
-        pass
-        
-        
-    def get_title(self):
-        return self.title
-        
-    def get_read_val(self):
-        val = self.bus.read_i2c_block_data(self.i2c_address, self.reg_temp, 2)
-        temp_c = (val[0] << 8) | (val[1] )
-        temp_c = temp_c * 0.0078125
-        return temp_c
+from smbus2 import SMBus
 
-        
-    def is_measuring(self):
-        return False
-        
+i2c_ch = 1
+reg_temp = 0x00
+reg_config = 0x01
+bus = SMBus(i2c_ch)
+readable = True
+read_val = 0.0
+
+def TMP117(address):
+    val = bus.read_i2c_block_data(address, reg_config, 2)
+    val[1] = val[1] & 0b00111111
+    val[1] = val[1] | (0b10 << 6)
+    bus.write_i2c_block_data(address, reg_config, val)
+    val = bus.read_i2c_block_data(address, reg_config, 2)
+    temp_c = (val[0] << 8) | (val[1] )
+    temp_c = temp_c * 0.0078125
+    return temp_c
+
+ 
 #ADRmu1_temp_sensor=TMP117(address=0x48, title="ADRmu1 Temp Sensor")
 #ADRmu2_temp_sensor=TMP117(address=0x4B, title="ADRmu2 Temp Sensor")
-#ADRmu3_temp_sensor=TMP117(address=0x4A, title="ADRmu3 Temp Sensor")
+ADRmu3_temp_sensor=TMP117(address=0x4A)
 #ADRmu4_temp_sensor=TMP117(address=0x49, title="ADRmu4 Temp Sensor")
-print(ADRmu4_temp_sensor.get_read_val())
+print(TMP117(0x4A))
