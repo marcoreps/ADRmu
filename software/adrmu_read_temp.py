0d232657c882b1ad3eb015c7f6ad8719bb89adff
reps <marco@reps.cc>
2023-03-31 09:24:50 +0200

    Update adrmu_read_temp.py

--- software/adrmu_read_temp.py
+++ software/adrmu_read_temp.py
@@ -1,25 +1,8 @@
-from smbus2 import SMBus
+# https://docs.circuitpython.org/projects/tmp117/en/latest/index.html
 
-i2c_ch = 1
-reg_temp = 0x00
-reg_config = 0x01
-bus = SMBus(i2c_ch)
-readable = True
-read_val = 0.0
-
-def TMP117(address):
-    val = bus.read_i2c_block_data(address, reg_config, 2)
-    val[1] = val[1] & 0b00111111
-    val[1] = val[1] | (0b10 << 6)
-    bus.write_i2c_block_data(address, reg_config, val)
-    val = bus.read_i2c_block_data(address, reg_config, 2)
-    temp_c = (val[0] << 8) | (val[1] )
-    temp_c = temp_c * 0.0078125
-    return temp_c
-
- 
-#ADRmu1_temp_sensor=TMP117(address=0x48, title="ADRmu1 Temp Sensor")
-#ADRmu2_temp_sensor=TMP117(address=0x4B, title="ADRmu2 Temp Sensor")
-ADRmu3_temp_sensor=TMP117(address=0x4A)
-#ADRmu4_temp_sensor=TMP117(address=0x49, title="ADRmu4 Temp Sensor")
-print(TMP117(0x4A))
+import board
+import adafruit_tmp117
+i2c = board.I2C()
+address = 0x4B
+tmp117 = adafruit_tmp117.TMP117(i2c, address)
+print("Temperature: %.2f degrees C"%tmp117.temperature)
